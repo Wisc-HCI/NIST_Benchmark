6b70899ebdfae3158286da49aed24a4737a7854c
Mya Schroder <schromya@gmail.com>
2024-11-01 13:11:54 -0500

    Hard-coded pick and place actually working reliably!

--- src/panda_benchmark/scripts/pick_and_place.py
+++ src/panda_benchmark/scripts/pick_and_place.py
@@ -13,7 +13,9 @@ def move_to_cartesian_position_multiple_attempts(
     and at orientation roll (rotation around x-axis), pitch (rotation around y-axis), and yaw 
     (rotation around z-axis) in radians.
     """
-    ATTEMPTS = 3
+
+    # TODO: Find smatter way than just doing this a bunch of times
+    ATTEMPTS = 8
     for _ in range(ATTEMPTS):
         move_to_cartesian_position(arm, 
                                     x, y, z, # X, Y, Z in m
@@ -29,30 +31,32 @@ if __name__ == '__main__':
     
     # Move to bolt
     move_to_cartesian_position_multiple_attempts(arm, 
-                              0.425, 0.0955, 0.2, # X, Y, Z in m
+                              0.44, 0.0955, 0.2, # X, Y, Z in m
                               0, 0.0, 0.0) # Roll, Pitch, Yaw in rads
     
     # Grab bolt
-    arm.exec_gripper_cmd(0.015) 
+    # TODO: Need smarter grip code that goes until times out or senses force or something (can't grip hard things)
+    arm.exec_gripper_cmd(0.02) 
 
     # Move up
     move_to_cartesian_position_multiple_attempts(arm, 
-                            0.425, 0.0955, 0.35, # X, Y, Z in m
+                            0.44, 0.0955, 0.35, # X, Y, Z in m
                             0, 0.0, 0.0) # Roll, Pitch, Yaw in rads
     
 
 
     # Move across board
     move_to_cartesian_position_multiple_attempts(arm, 
-                        0.29, -0.21, 0.35, # X, Y, Z in m
+                        0.3, -0.215, 0.35, # X, Y, Z in m
                         0, 0.0, 0.0) # Roll, Pitch, Yaw in rads
 
     # Move down board
     move_to_cartesian_position_multiple_attempts(arm, 
-                        0.29, -0.21, 0.2, # X, Y, Z in m
+                        0.3, -0.215, 0.21, # X, Y, Z in m
                         0, 0.0, 0.0) # Roll, Pitch, Yaw in rads
 
     # Release bolt
+    arm.exec_gripper_cmd(0.07)
     arm.exec_gripper_cmd(0.07)
 
     # Return to neutral position
